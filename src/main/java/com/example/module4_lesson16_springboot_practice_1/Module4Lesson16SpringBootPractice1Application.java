3ccd50f1a142c0026597ee5fc4205c39db2cdf3e
hoale0507 <hoale090293@gmail.com>
2022-04-09 22:47:30 +0700

    add file

---
+++ src/main/java/com/example/module4_lesson16_springboot_practice_1/Module4Lesson16SpringBootPractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.module4_lesson16_springboot_practice_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4Lesson16SpringBootPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4Lesson16SpringBootPractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/module4_lesson16_springboot_practice_1/Module4Lesson16SpringBootPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module4_lesson16_springboot_practice_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4Lesson16SpringBootPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
